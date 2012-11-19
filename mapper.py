9bac3bd9efa52a0dad3ec90dce1f1e54c48dcf32
Zhaojie Zhang <gongchangzhaojie@yahoo.com>
2012-11-19 16:42:48 -0700

    change to the mapper.py:len(country)

--- mapper.py
+++ mapper.py
@@ -4,6 +4,11 @@ import sys
 
 for line in sys.stdin:
     record=line.strip(" \r\n").split(",")
-    country=record[4]
-    if len(country)==2:
+    #state is the 6th field of the record
+    country=record[5]
+    #print record
+    
+    #the length of the state abbreviation is 2, but " and " will have
+    #a size of 1 each.Therefore, the total size should be 4
+    if len(country)==4:
         print country,1
