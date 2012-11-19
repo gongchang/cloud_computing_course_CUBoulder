1bf59fe3d2ad6a4df9868e3640619f677f5deebf
Zhaojie Zhang <gongchangzhaojie@yahoo.com>
2012-11-19 16:51:25 -0700

    changes to the reducer.py

--- reducer.py
+++ reducer.py
@@ -4,13 +4,13 @@ import sys
 dic1={}
 
 for line in sys.stdin:
-    record=line.strip(" \r\n").split("\t")
+    record=line.strip(" \r\n").split(" ")
     country=record[0]
     count=int(record[1])
     if country not in dic1:
         dic1[country]=count
     else:
-        dic[country]+=1
+        dic1[country]+=1
 
 for key,value in dic1.iteritems():
     print key,value
